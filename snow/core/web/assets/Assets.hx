850c17e6a51649dd16a8f7e584eca896008d24e8
Jeremy Faivre <contact@jeremyfa.com>
2019-04-18 11:25:17 +0200

    web; Add snow_web_use_electron_pngjs flag to allow to use electron’s pngjs module to decode png data synchronously

--- snow/core/web/assets/Assets.hx
+++ snow/core/web/assets/Assets.hx
@@ -15,6 +15,12 @@ import snow.core.web.assets.psd.PSD;
 @:allow(snow.systems.assets.Assets)
 class Assets implements snow.modules.interfaces.Assets {
 
+    #if snow_web_use_electron_pngjs
+    var tested_electron_pngjs_availability:Bool = false;
+    var electron:Dynamic = null;
+    var pngjs:Dynamic = null;
+    #end
+
 //module interface
 
     var app: snow.Snow;
@@ -56,6 +62,43 @@ class Assets implements snow.modules.interfaces.Assets {
 
         } //image_info_from_element
 
+        #if snow_web_use_electron_pngjs
+
+            /** Create an image info (padded to POT) from a given Canvas or Image element. */
+        public function image_info_from_pngjs_info( _id:String, _pngjs_info:Dynamic ) : ImageData {
+
+            var width_pot = nearest_power_of_two(_pngjs_info.width);
+            var height_pot = nearest_power_of_two(_pngjs_info.height);
+
+            // Copy data and get rid of nodejs buffer
+            var buffer_data = _pngjs_info.data;
+            var pngjs_data = new Uint8Array(buffer_data.length);
+            for (i in 0...buffer_data.length) {
+                pngjs_data[i] = untyped __js__("{0}[{1}]", buffer_data, i);
+            }
+
+            var image_bytes = POT_bytes_from_pixels(_pngjs_info.width, _pngjs_info.height, width_pot, height_pot, pngjs_data);
+
+            var info = new ImageData(app, {
+                id : _id,
+                bpp : 4,
+                width : _pngjs_info.width,
+                height : _pngjs_info.height,
+                width_actual : width_pot,
+                height_actual : height_pot,
+                bpp_source : 4,
+                pixels : image_bytes
+            });
+
+            image_bytes = null;
+            buffer_data = null;
+
+            return info;
+
+        } //image_info_from_pngjs_info
+
+        #end
+
             /** Create an image info from raw (already decoded) image pixels */
         public function image_info_from_pixels(_id:String, _width:Int, _height:Int, _pixels:Uint8Array, ?_bpp:Int=4) : ImageData {
 
@@ -88,6 +131,33 @@ class Assets implements snow.modules.interfaces.Assets {
 
             #if snow_web_psd
                 if(ext == 'psd') return Promise.resolve(image_info_from_bytes_psd(_id , _bytes));
+            #end
+
+            #if snow_web_use_electron_pngjs
+            if (!tested_electron_pngjs_availability) {
+                tested_electron_pngjs_availability = true;
+                try {
+                    electron = untyped __js__("require('electron')");
+                    pngjs = untyped __js__("{0}.remote.require('pngjs')", electron);
+                }
+                catch (e:Dynamic) {}
+            }
+
+            if (pngjs != null && ext == 'png') {
+                return new Promise(function(resolve, reject) {
+
+                    try {
+                        var Buffer = untyped __js__("{0}.remote.require('buffer').Buffer", electron);
+                        var pngjs_info = pngjs.PNG.sync.read(Buffer.from(_bytes));
+
+                        resolve(image_info_from_pngjs_info(_id, pngjs_info));
+                    }
+                    catch (e:Dynamic) {
+                        reject(Error.error('failed to decode png: $_id ; $e'));
+                    }
+
+                });
+            }
             #end
 
             return new Promise(function(resolve, reject) {
